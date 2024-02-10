3b0ad337b845e2eabf31a873fb63b147c480221b
Gordon999 <Gordon999@users.noreply.github.com>
2024-02-10 15:59:48 +0000

    v0.05

--- Pifastraws.py
+++ Pifastraws.py
@@ -10,12 +10,12 @@ import cv2
 import numpy as np
 import shutil
 
-#v0.03
+#v0.05
 
 # setup
-framerate  = 100   # fps
-pre_frames = 100   # Minimum Number of PRE Frames
-v_length   = 5000  # in mS
+framerate  = 200   # fps
+pre_frames = 100   # Number of PRE Frames
+cap_length = 5000  # in mS
 ram_limit  = 150   # in MB, stops if RAM below this
 width      = 640   # frame width 
 height     = 480   # frame height
@@ -30,12 +30,12 @@ pic_dir = "/home/" + Home_Files[0]+ "/Pictures/"
 
 # clear ram
 print("Clearing RAM...")
-pics = glob.glob('/run/shm/*.raw')
-for tt in range(0,len(pics)):
-    os.remove(pics[tt])
-pics = glob.glob('/run/shm/*.jpg')
-for tt in range(0,len(pics)):
-    os.remove(pics[tt])
+frames = glob.glob('/run/shm/*.raw')
+for tt in range(0,len(frames)):
+    os.remove(frames[tt])
+frames = glob.glob('/run/shm/*.jpg')
+for tt in range(0,len(frames)):
+    os.remove(frames[tt])
    
 # start camera with subprocess
 print("Starting Camera...")
@@ -47,8 +47,8 @@ while poll != None:
     poll = s.poll()
 
 print("Capturing Pre-Frames...")
-while len(pics) < pre_frames:
-    pics = glob.glob('/run/shm/temp*.raw')
+while len(frames) < pre_frames:
+    frames = glob.glob('/run/shm/temp*.raw')
 print("Pre-Frames captured...")
 print("Ready for Trigger.....")
 
@@ -62,34 +62,32 @@ while freeram > ram_limit:
     # check ram and stop if full
     st = os.statvfs("/run/shm/")
     freeram = (st.f_bavail * st.f_frsize)/1100000
-    if freeram < ram_limit:
-        run = 1
         
     # read temp files
-    pics = glob.glob('/run/shm/temp*.raw')
-    # DELETE OLD FRAMES
-    for tt in range(pre_frames,len(pics)-1):
-        os.remove(pics[tt])
+    frames = glob.glob('/run/shm/temp*.raw')
+    # DELETE old frames from buffer
+    for tt in range(pre_frames,len(frames)-1):
+        os.remove(frames[tt])
    
     # trigger from button
     if trigger.is_pressed:
         now = datetime.datetime.now()
         timestamp = now.strftime("%y%m%d_%H%M%S_%f")
-        w = len(pics)
+        w = len(frames)
         print("Triggered...", timestamp)
        
-        # capture for v_length
+        # capture for cap_length
         start = time.monotonic()
         st = os.statvfs("/run/shm/")
         freeram = (st.f_bavail * st.f_frsize)/1100000
-        while time.monotonic() - start < v_length/1000 and freeram > ram_limit:
+        while time.monotonic() - start < cap_length/1000 and freeram > ram_limit:
             st = os.statvfs("/run/shm/")
             freeram = (st.f_bavail * st.f_frsize)/1100000
         
         # stop camera subprocess
         os.killpg(s.pid, signal.SIGTERM)
         
-        # rename frames 
+        # get date parameters 
         yr = timestamp[0:2]
         mh = timestamp[2:4]
         dy = timestamp[4:6]
@@ -98,12 +96,13 @@ while freeram > ram_limit:
         sc = int(timestamp[11:13])
         ms = int(timestamp[14:21])
         dz = timestamp[0:6]
-        fx = 1
-        pics = glob.glob('/run/shm/temp*.raw')
-        pics.reverse()
+
+        # get frames list
+        frames = glob.glob('/run/shm/temp*.raw')
+        frames.reverse()
         
         # calculate times for file name
-        for x in range(0,len(pics)):
+        for x in range(0,len(frames)):
             mc = ms + ((x - w) * int(1000000/framerate))
             sc2 = sc
             mn2 = mn
@@ -140,13 +139,12 @@ while freeram > ram_limit:
             hs = str(hr2)
             if len(hs) < 2:
                 hs = "0" + hs
-            timestamp = hs+mo+sd+"_"+str(md)[-6:]
+            timestamp = hs + mo + sd + "_" + str(md)[-6:]
             if x == w:
                 trig = timestamp
-            # rename file
-            if os.path.exists(pics[x]):
-                os.rename(pics[x],pics[x][0:9] + timestamp + '.raw')
-                fx +=1
+            # rename file to HHMMSS-mmmmmm.raw
+            if os.path.exists(frames[x]):
+                os.rename(frames[x],frames[x][0:9] + timestamp + '.raw')
        
         # show trigger frame
         fd = open('/run/shm/' + trig + '.raw', 'rb')
@@ -159,18 +157,18 @@ while freeram > ram_limit:
        
         # clear ram of temp files
         print("Clearing temp files from RAM...")
-        pics = glob.glob('/run/shm/temp*.raw')
-        for tt in range(0,len(pics)):
-            os.remove(pics[tt])
+        frames = glob.glob('/run/shm/temp*.raw')
+        for tt in range(0,len(frames)):
+            os.remove(frames[tt])
 
         # move RAM Files to SD card
         print("Moving files to SD card (/Pictures)...")
         if not os.path.exists(pic_dir + dz) :
             os.system('mkdir ' + pic_dir + dz)
-        vpics = glob.glob('/run/shm/*.raw')
-        for xx in range(0,len(vpics)):
-             if not os.path.exists(pic_dir + vpics[xx][9:]) and vpics[xx][0:4] != "temp":
-                 shutil.move(vpics[xx],pic_dir + dz)
+        vframes = glob.glob('/run/shm/*.raw')
+        for xx in range(0,len(vframes)):
+             if not os.path.exists(pic_dir + vframes[xx][9:]) and vframes[xx][0:4] != "temp":
+                 shutil.move(vframes[xx],pic_dir + dz)
 
         # wait for a key press
         print( "Press ESC to exit, Any other key to restart")
@@ -190,8 +188,8 @@ while freeram > ram_limit:
             print("waiting...")
             poll = s.poll()
         print("Capturing Pre-Frames...")
-        while len(pics) < pre_frames:
-            pics = glob.glob('/run/shm/temp*.raw')
+        while len(frames) < pre_frames:
+            frames = glob.glob('/run/shm/temp*.raw')
         print("Pre-Frames captured...")
         print("Trigger when ready...")
             
